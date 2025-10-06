f4ba9e528ec9d0e4a7e697fb8de8d92026f72cf3
coconhat <nhat_vu_le@dlsl.edu.ph>
2025-10-06 21:24:14 +0800

    fix: correct variable name for registration key

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ function isValid(object) {
 }
 
 async function getStudentInfo(id) {
-    const regKey = process.env.REG_KEY;
-    if (!regKey) {
-        throw new Error('REG_KEY environment variable is not set');
-    }
+  const regKey = process.env.REG_KEY;
+  if (!regKey) {
+    throw new Error("REG_KEY environment variable is not set");
+  }
   const api = "https://portal.dlsl.edu.ph/registration/event/helper.php";
 
   try {
@@ -47,7 +47,7 @@ async function getStudentInfo(id) {
       },
       body: new URLSearchParams({
         action: "registration_tapregister",
-        regkey: regkey,
+        regkey: regKey,
         card_tag: id,
       }),
     });
